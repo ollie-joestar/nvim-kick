9ab581e3eb565d671c9d84c8916c26f4f2517b13
Oleh Ohnivchuk <oohnivch@c3r3p1.42vienna.com>
2024-10-11 18:00:56 +0200

    first commit

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,9 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+-- require 'catppuccin'
+-- require 'joestar.plugins.lualine'
+-- require 'joestar.plugins.42'
+-- require 'joestar.plugins.presence'
+return {}
